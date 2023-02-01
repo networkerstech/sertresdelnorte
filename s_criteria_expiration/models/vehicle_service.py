fe5d8e873d613333dae22a5fac5c1541f9ea67c8
Carlos Maykel <cmlg.cu@gmail.com>
2023-02-01 12:40:42 -0500

    [ADD] Se adiciona el s_criteria_expiration.

---
+++ s_criteria_expiration/models/vehicle_service.py
@@ -0,0 +1,13 @@
+from odoo import fields, models, api
+
+
+class VehicleService(models.Model):
+    _name = 'vehicle.service'
+    _description = 'Vehicle service'
+    _rec_name = 'services_id'
+
+    services_id = fields.Many2one('fleet.service.type', store=True)
+    responsible_id = fields.Many2one('res.users', string='Responsible', store=True, readonly=False)
+    criteria_days = fields.Integer(string='Criteria Days', help="Quantity expressed in days")
+    criteria_km = fields.Integer(string='Criteria KMs', help="Quantity expressed in KM")
+    active = fields.Boolean(string='Active', default=True)
