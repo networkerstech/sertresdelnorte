d9c40d6ca426f56ed1dc5188a935fd7a3470e4f3
Carlos Maykel <cmlg.cu@gmail.com>
2023-02-13 23:57:18 -0500

    [MOD] MOdificaciones al modulo s_insure_fleet a solicitud del cliente.

--- s_insure_fleet/models/inherit_fleet_vehicle.py
+++ s_insure_fleet/models/inherit_fleet_vehicle.py
@@ -1,62 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _
-from dateutil.relativedelta import relativedelta
-from odoo.tools import format_date
 
 
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
-    insurance_start_date = fields.Date(string="Start date")
-    insurance_end_date = fields.Date(string="End date")
-    insurance_scheduled_activity = fields.Boolean(default=False)
+    activity_line_id = fields.One2many(comodel_name="vehicle.activity", inverse_name="vehicle_id", string="Activity")
 
-    cargo_start_date = fields.Date(string="Start date")
-    cargo_end_date = fields.Date(string="End date")
-    cargo_scheduled_activity = fields.Boolean(default=False)
-
-
-    @api.model
-    def _cron_check_insurance_policy_validity(self):
-
-        insurance = self.env['insurance.policy'].search([], limit=1)
-        outdated_days = fields.Date.today() + relativedelta(days=+insurance.criteria_days)
-        employee = insurance.responsible_id
-
-        expired_insurance_policy = self.search([('insurance_scheduled_activity', '=', False), ('insurance_end_date', '<', outdated_days)])
-
-        for vehicle in expired_insurance_policy:
-            if employee:
-                lang = self.env['res.partner'].browse(employee.id).lang
-                formated_date = format_date(vehicle.env, vehicle.insurance_end_date, date_format="dd MMMM y", lang_code=lang)
-                vehicle.activity_schedule(
-                    'mail.mail_activity_data_todo',
-                    note=_('The insurance policy for the vehicles with registration %(vehicle)s expires expires at %(date)s.',
-                           vehicle=vehicle.license_plate,
-                           date=formated_date),
-                    user_id=employee.id)
-
-            vehicle.write({'insurance_scheduled_activity': True})
-
-    @api.model
-    def _cron_check_cargo_permit_validity(self):
-
-        cargo = self.env['cargo.permit'].search([], limit=1)
-        outdated_days = fields.Date.today() + relativedelta(days=+cargo.criteria_days)
-        employee = cargo.responsible_id
-
-        expired_cargo_permit = self.search([('insurance_scheduled_activity', '=', False), ('cargo_end_date', '<', outdated_days)])
-
-        for vehicle in expired_cargo_permit:
-            if employee:
-                lang = self.env['res.partner'].browse(employee.id).lang
-                formated_date = format_date(vehicle.env, vehicle.cargo_end_date, date_format="dd MMMM y", lang_code=lang)
-                vehicle.activity_schedule(
-                    'mail.mail_activity_data_todo',
-                    note=_('The cargo permit of %(vehicle)s expires at %(date)s.',
-                           vehicle=vehicle.license_plate,
-                           date=formated_date),
-                    user_id=employee.id)
-
-            vehicle.write({'cargo_scheduled_activity': True})
---
+++ s_insure_fleet/models/vehicle_activity.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api, _
+from dateutil.relativedelta import relativedelta
+from odoo.tools import format_date
+from datetime import datetime
+
+
+class VehicleActivity(models.Model):
+    _name = 'vehicle.activity'
+    _description = 'Description'
+
+    vehicle_id = fields.Many2one(comodel_name="fleet.vehicle", string="Vehicle")
+    activity_ids = fields.Many2one(comodel_name="activity.parameters", string="Activity")
+    start_date = fields.Date(string="Start date")
+    end_date = fields.Date(string="End date")
+    scheduled_activity = fields.Boolean(string="scheduled activity", defaul="False")
+
+    def write(self, vals):
+        if "end_date" in vals and self.end_date < datetime.strptime(vals.get("end_date"), '%Y-%m-%d').date():
+            vals.update({
+                'scheduled_activity': False
+            })
+
+        res = super(VehicleActivity, self).write(vals)
+
+    @api.model
+    def _cron_check_activity_validity(self):
+
+        for act in self.env["activity.parameters"].search([]):
+            out_dated = fields.Date.today() + relativedelta(days=+act.criteria_days)
+            responsible = act.responsible_id
+
+            expired_activity = self.search(
+                [
+                    ('scheduled_activity', '=', False),
+                    ('end_date', '<=', out_dated)
+                ]
+            ).filtered(lambda x: x.activity_ids == act)
+
+            for rec in expired_activity:
+                if responsible:
+                    lang = self.env['res.partner'].browse(responsible.id).lang
+                    formated_date = format_date(rec.env, rec.end_date, date_format="dd MMMM y", lang_code=lang)
+
+                    vehicle = self.env["fleet.vehicle"].search([]).filtered(lambda x: x.id == rec.vehicle_id.id)
+
+                    vehicle.activity_schedule(
+                        'mail.mail_activity_data_todo',
+                        note=_('%(activity)s expires on date %(date)s for the vehicle with registration %(vehicle)s.',
+                               activity=act.parameter,
+                               date=formated_date,
+                               vehicle=vehicle.license_plate),
+                        user_id=responsible.id
+                    )
+
+                rec.write({
+                    'scheduled_activity': True,
+                })
