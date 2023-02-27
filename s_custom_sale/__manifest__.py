064dcd69465c0e1ca46f20268a210a50acbeddb0
Carlos Maykel <cmlg.cu@gmail.com>
2023-02-27 09:12:44 -0500

    [MOD] Se modifica el modulo s_custom_sale para cumplir restriciones del cliente

--- s_custom_sale/__manifest__.py
+++ s_custom_sale/__manifest__.py
@@ -8,7 +8,7 @@
     'website': "https://www.suitedoo.com",
     'category': 'sale',
     'version': '0.1',
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale_management'],
     'data': [
         'views/inherit_sale_order_views.xml',
         'views/inherit_analytic_account_views.xml'
--- s_custom_sale/models/inherit_sale_order.py
+++ s_custom_sale/models/inherit_sale_order.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
-
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 READONLY_FIELD_STATES = {
     state: [('readonly', True)]
     for state in {'sale', 'done', 'cancel'}
 }
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -24,3 +25,24 @@ class SaleOrder(models.Model):
         help="Delivery Need Date.",
         default=fields.Datetime.now
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            flag = False
+            account_analytic = ""
+            if vals.get('document_type') in "requisitions":
+                for order in vals.get('order_line'):
+                    analytic = self.env['account.analytic.account'].search([('id', '=', order[2].get('analytic_account_id'))])
+                    if analytic.project_state not in 'running':
+                        flag = True
+                        account_analytic = analytic.name
+
+        if flag:
+            raise ValidationError(
+                _(
+                    "It cannot be processed because the project status of the analytical account %s is Finished" % account_analytic
+                )
+            )
+        else:
+            return super(SaleOrder, self).create(vals_list)
