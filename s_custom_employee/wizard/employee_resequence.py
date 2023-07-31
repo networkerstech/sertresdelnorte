a9da734d6c235dafa816eded9ce8d90c9bbb129c
Alfredo <alfredo.bellotelles@gmail.com>
2023-07-31 15:38:00 -0400

    IMP-s_custom_employee-improve_sequence-DEV_ABT Cambiar formato de secuencia para empleados Asistente para resecuenciar empleados

---
+++ s_custom_employee/wizard/employee_resequence.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import _, models
+from odoo.exceptions import AccessError
+
+class EmployeeReSequenceWizard(models.TransientModel):
+    _name = 'employee.resequence.wizard'
+    _description = 'Remake the sequence of employees.'
+
+    def resequence(self):
+        sequence = self.env.ref(
+            's_custom_employee.hr_employee_employee_sequence_number')
+
+        # Force sequence restart
+        sequence.write({
+            'implementation': sequence.implementation,
+            'number_next': 1,
+        })
+
+        employees = self.env['hr.employee'].search([])
+        for employee in employees:
+            employee.number_employee = sequence._next()
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
