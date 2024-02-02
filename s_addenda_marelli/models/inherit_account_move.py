13f70cebd42cd161b21386642d8d5020f2fa03d6
Eddy <epvila93@gmail.com>
2024-02-02 11:54:06 -0500

    [ADD] agregar modulo s_addenda_levis

--- s_addenda_marelli/models/inherit_account_move.py
+++ s_addenda_marelli/models/inherit_account_move.py
@@ -23,15 +23,3 @@ class AccountMove(models.Model):
         string="Use Addenda Marelli",
         related='partner_id.partner_use_addenda_marelli',
     )
-    
-    def w_get_cfdi_values(self):
-        self.ensure_one()
-        edi=self._get_l10n_mx_edi_signed_edi_document()
-        values=edi.edi_format_id._l10n_mx_edi_get_serie_and_folio(self)
-        if self._context.get('get_folio', False):
-            folio = values.get('folio_number', False) or ''
-            return folio
-        elif self._context.get('get_serie', False):
-            serie = values.get('serie_number', False) or ''
-            return serie
-
