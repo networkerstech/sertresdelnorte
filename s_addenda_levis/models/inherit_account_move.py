25dc5b37f59c3aa3e37803aff7cc7892cf5369aa
Eddy <epvila93@gmail.com>
2024-02-03 11:53:11 -0500

    [ADD] agregar modulo s_addenda_levis

--- s_addenda_levis/models/inherit_account_move.py
+++ s_addenda_levis/models/inherit_account_move.py
@@ -9,3 +9,21 @@ class AccountMove(models.Model):
         string="Use Addenda Levi's",
         related='partner_id.partner_use_addenda_levis',
     )
+
+    def _get_sale_order_levi(self):
+        sale_order = self.invoice_line_ids.mapped('sale_line_ids').mapped('order_id')
+        if sale_order:
+            return sale_order[0].name
+        return ''
+    
+    def _get_efective_date_levi(self):
+        sale_order = self.invoice_line_ids.mapped('sale_line_ids').mapped('order_id')
+        if sale_order:
+            return sale_order[0].effective_date.strftime('%Y-%m-%d')+'T'+sale_order[0].effective_date.strftime("%H:%M:%S")
+        return ''
+    
+    def _get_sale_order_line_levi(self):
+        sale_order = self.invoice_line_ids.mapped('sale_line_ids').mapped('order_id')
+        if sale_order:
+            return sale_order[0].order_line
+        return []
